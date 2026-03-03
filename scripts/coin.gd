ebe67994b378a87fada1e4b1c5881c5b97c9cff7
jcartwright2 <jcartwright2@ftstudent.org>
2026-03-03 14:07:40 -0500

    upd 3/3
    upd

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,11 +1,9 @@
 extends Area2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print("I'm a coin")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+func _on_body_entered(body: Node2D) -> void:
+	print("+1 coin")
+	queue_free()
